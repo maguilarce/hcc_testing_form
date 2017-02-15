dcf5175b8f09890666c7eaabd6c96153fb49055d
manuel.aguilar2 <manuel.aguilar2@sy3e09AHccOnlineprg.ad.hccs.edu>
2017-02-15 16:17:10 -0600

    adding session and users functionality 3

--- process_crn.php
+++ process_crn.php
@@ -2,25 +2,24 @@
 include('dconnection.php');
 $course = $_POST['offered_course'];
 
-if(!$course) {
+if (!$course) {
 
-	return false;
+    return false;
 }
 
 
 $sql = "SELECT CRN FROM courses WHERE Title = '$course' and Term = 6172";
 $result = $mysqli->query($sql);
-
 ?>
 
 <label>CRN</label><i class="fa fa-asterisk text-danger">*</i>
 <select multiple name="crn[]" class="form-control" required>
-	<option value="">Please Select</option>
-	<?php
-	while ($crn = $result->fetch_assoc()) {
-		?>
-		<option value="<?php echo $crn['CRN']; ?>"><?php echo $crn['CRN']; ?></option>
-		<?php 
-	}
-	?>
+    <option value="">Please Select</option>
+    <?php
+    while ($crn = $result->fetch_assoc()) {
+        ?>
+        <option value="<?php echo $crn['CRN']; ?>"><?php echo $crn['CRN']; ?></option>
+        <?php
+    }
+    ?>
 </select>
